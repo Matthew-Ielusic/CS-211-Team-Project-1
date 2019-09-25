b5658f1343aba59b109e0402175cf3f54c178ff6
Matthew-Ielusic <matthew38@optonline.net>
2019-09-25 14:35:44 -0700

    Initial commit -- add partial implementation of Person

---
+++ Source/Person.java
@@ -0,0 +1,28 @@
+// Author: Matt Ielusic
+
+import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
+
+public class Person {
+
+   private Person mother;
+   private Person father;
+   private ArrayList<Person> children;
+   
+   public Person() {
+      this(null, null);
+   }
+   
+   public Person(Person mother, Person father) {
+      this.mother = mother;
+      this.father = father;
+      this.children = new ArrayList<>();
+   }
+   
+   public void addChild(Person newChild) {
+      if (newChild == null) {
+         throw new IllegalArgumentException();
+      }
+      this.children.add(newChild);
+   }
+}

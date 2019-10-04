5882d52ca9491c3cf0ad695add559899c3753829
Matthew-Ielusic <matthew38@optonline.net>
2019-10-03 21:21:27 -0700

    Reformat comments & spacing
     * The code of FileParser and Person should now have consitent spacing, better comments, and better follow the requirements on Canvas
     * This commit should not change any behavior

--- Source/FileParser.java
+++ Source/FileParser.java
@@ -1,97 +1,95 @@
+/* 
+ * Matt Ielusic & Nick Schleiff
+ * CS 211
+ * 10/4/2019
+ * Team Project #1
+ * FileParser is responsible for taking a file, as described in the problem description,
+ * and figuring out what the family tree is.
+ */
+
 import java.util.*;
 import java.io.*;
 
-// MATT: tudor.dat has two parts.  The first part is an unordered list of people, and the second part
-// is the list of people, ordered [Person, their mother, their father] [Someone else, that person's mother, that person's father], etc.
-// So I think parsing the file should be done in two steps:
-// First, use the first half of the file to figure out who the people are
-// Second, use the second half of the file to figure out how the people we calculated in step 1 are related.
-
-//NICK: The method "calulateFamilyTree" does so by adding mothers and fathers to the instances found in out listOfPeople.
-//it also adds them to familyTree. In that way, both arrayLists reflect the same data
-        
+/* The data files have two parts.  The first part is an unordered list of people, and the second part
+ * is the list of people, ordered [Person, their mother, their father] [Someone else, that person's mother, that person's father], etc.
+ * So I think parsing the file should be done in two steps:
+ * First, use the first half of the file to figure out who the people are
+ * Second, use the second half of the file to figure out how the people we calculated in step 1 are related.
+ * -Matt
+ */        
 
 public class FileParser {
-    private File file;
     private ArrayList<Person> people;
 
     private static final String DELIMITER_BETWEEN_PARTS_OF_FILE = "END";
     
     public FileParser (File file) throws FileNotFoundException {
-        // Initialize fields
-        this.file = file;
+        // Initialize fields - Matt
         this.people = new ArrayList<>();
 
         Scanner scanner = new Scanner(file);               
         
-        calculateListOfPeople(scanner);  //NICK: Parses first part of file
-        calculateFamilyTree(scanner); //NICK: Parses second part
+        calculateListOfPeople(scanner);  // Parses first part of file -Nick
+        calculateFamilyTree(scanner); //Parses second part -Nick
+    }
+    
+    // No-arg constructor as required by the Requirements for Class (Data Structure) Submission page on Canvas - Matt
+    public FileParser() {
+        this.people = new ArrayList<>();
+    }
+    
+    public ArrayList<Person> getPeople() {
+        return people;
     }
     
     private void calculateListOfPeople(Scanner fileScanner) {
-      // Precondition: fileScanner is pointing to the start of the input file
-      String line = fileScanner.nextLine();
+        // Precondition: fileScanner is pointing to the start of the input file - Matt
+        // Postcondition: fileScanner is pointing to the line after the first occurence of DELIMITER_BETWEEN_PARTS_OF_FILE - Matt
+        String line = fileScanner.nextLine();
       
-      while (!line.equals(DELIMITER_BETWEEN_PARTS_OF_FILE)) {
-         Person newPerson = new Person(line);
-         this.people.add(newPerson);
+        while (!line.equals(DELIMITER_BETWEEN_PARTS_OF_FILE)) {
+            Person newPerson = new Person(line);
+            this.people.add(newPerson);
          
-         line = fileScanner.nextLine();
-      }
-    }
-
-    
-    //private method to avoid being called twice
-    private void calculateFamilyTree(Scanner scan) {
-      while (scan.hasNextLine()) {
-        String next = scan.nextLine();
-
-        if(!next.equals("END")) {
-          Person p = getPersonFromString(next);
-
-          if(scan.hasNextLine()) {
-            Person mother = getPersonFromString(scan.nextLine());
-            if (mother != null) {
-                p.setMother(mother);       
-                mother.addChild(p);   
-          }
-          if(scan.hasNextLine()) {
-            Person father = getPersonFromString(scan.nextLine());
-            if (father != null) {
-                p.setFather(father);
-                father.addChild(p);
-            }
-          }
-        } 
-      }
-    }
-    }
-    
-    private void calculateChildren() {
-      for (Person child : people) {
-         Person mother = child.getMother();
-         if (mother != null) {
-            mother.addChild(child);
-         }
-         Person father = child.getFather();
-         if (father != null) {
-            father.addChild(child);
-         }
-      }
-   }
-        
-
-   public ArrayList<Person> getPeople() {
-      return people;
-   }
-
-    //If person of given name is not found, null is returned
-    private Person getPersonFromString(String name) {
-      for (int i=0; i < people.size(); i++) {
-        if(people.get(i).toString().equals(name)) {
-          return people.get(i);
+            line = fileScanner.nextLine();
         }
-      }
-      return null;
     }
+
+    //private method to avoid being called twice - Nick
+    private void calculateFamilyTree(Scanner scan) {
+        // Precondition: scan is pointing to the line after the first occurence of DELIMITER_BETWEEN_PARTS_OF_FILE - Matt
+        // Postcondition: scan.hasNextLine() returns false - Matt
+        while (scan.hasNextLine()) {
+            String next = scan.nextLine();
+
+            if(!next.equals(DELIMITER_BETWEEN_PARTS_OF_FILE)) {
+                Person p = getPersonFromString(next);
+
+                if(scan.hasNextLine()) {
+                    Person mother = getPersonFromString(scan.nextLine());
+                    if (mother != null) {
+                        p.setMother(mother);       
+                        mother.addChild(p);   
+                    }
+                    if(scan.hasNextLine()) {
+                        Person father = getPersonFromString(scan.nextLine());
+                        if (father != null) {
+                            p.setFather(father);
+                            father.addChild(p);
+                        }
+                    }
+                } 
+            }
+        }
+    }
+
+    //If person of given name is not found, null is returned - Nick
+    private Person getPersonFromString(String name) {
+        for (int i=0; i < people.size(); i++) {
+            if(people.get(i).toString().equals(name)) {
+                return people.get(i);
+            }
+        }
+     return null;
+   }
 }
--- Source/Person.java
+++ Source/Person.java
@@ -1,61 +1,68 @@
-// Author: Matt Ielusic
-
+/* 
+ * Matt Ielusic * CS 211
+ * 10/4/2019
+ * Team Project #1
+ * Simple class representing a person
+ */
 import java.util.ArrayList;
 import java.lang.IllegalArgumentException;
 
 public class Person {
 
-   private String name;
-   private Person mother;
-   private Person father;
-   private ArrayList<Person> children;
+    private String name;
+    private Person mother;
+    private Person father;
+    private ArrayList<Person> children;
    
-   // MATT: I now think my original set-of-constructors is wrong.
-   // The way the input file is structured, when we first instantiate a person,
-   // All we know is their name.
-   // The mother, father, and children must be set later.
-   public Person(String name) {
-      this.name = name;
-      this.children = new ArrayList<>();
-   }
-
-   public String toString() {
-      return name;
-   }
+    // The way the input file is structured, when we first instantiate a person,
+    // All we know is their name.
+    // The mother, father, and children must be set later. -Matt
+    public Person(String name) {
+        this.name = name;
+        this.children = new ArrayList<>();
+    }
    
-   // Getters
-
-   public Person getMother() {
-      return mother;
-   }
-
-   public Person getFather() {
-      return father;
-   }
-
-   public ArrayList<Person> getChildren() {
-      return children;
-   }
+    // No-arg constructor as required by the Requirements for Class (Data Structure) Submission page on Canvas - Matt
+    public Person() {
+        this("");
+    }
+    
+    public String toString() {
+       return name;
+    }
    
-   public String getName() {
-      return name;
-   }
-   
-   // Mutators
-   
-   public void setMother(Person newMom) {
-      this.mother = newMom;
-   }
+    // Getters
 
-   public void setFather(Person newDad) {
-      this.father = newDad;
-   }
-   
-   public void addChild(Person newChild) {
-      if (newChild == null) {
-         throw new IllegalArgumentException();
-      }
-      this.children.add(newChild);
-   }
+    public Person getMother() {
+       return mother;
+    }
 
+    public Person getFather() {
+       return father;
+    }
+
+    public ArrayList<Person> getChildren() {
+       return children;
+    }
+   
+    public String getName() {
+       return name;
+    }
+   
+    // Mutators
+   
+    public void setMother(Person newMom) {
+       this.mother = newMom;
+    }
+
+    public void setFather(Person newDad) {
+       this.father = newDad;
+    }
+   
+    public void addChild(Person newChild) {
+       if (newChild == null) {
+          throw new IllegalArgumentException();
+       }
+       this.children.add(newChild);
+    }
 }
